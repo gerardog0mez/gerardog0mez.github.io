690203bc84418ba09aec40bf3d5926cf2f66cea7
gerardog0mez <132062757+gerardog0mez@users.noreply.github.com>
2025-03-25 19:26:31 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,339 @@
+const translations = {
+    es: {
+        // ... (otros textos)
+        facebookText: "Síguenos en Facebook",
+        // Textos comunes
+        home: "Inicio",
+        products: "Productos",
+        contact: "Contacto",
+        beetchips: "BeetChips",
+        cornrose: "CornRose",
+        terms: "Términos y Condiciones",
+        copyright: "© NutriSnack. Todos los derechos reservados.",
+
+        // Index (NutriSnack)
+        missionTitle: "Misión",
+        missionText: "Compartir el verdadero sabor de México a través de snacks deliciosos, apoyando a los productores locales y celebrando nuestra cultura.",
+        visionTitle: "Visión",
+        visionText: "Ser una empresa exportadora reconocida internacionalmente, por su variedad de snacks saludables y compromiso con la sustentabilidad.",
+        valuesTitle: "Valores",
+        authenticity: "Autenticidad: Resaltamos la esencia de México en cada producto.",
+        sustainability: "Sostenibilidad: Por la utilización de empaques ecológicos.",
+        innovation: "Innovación: Evolucionamos para satisfacer las necesidades del mercado global.",
+        honesty: "Honestidad: Ser transparentes con los ingredientes que utilizamos.",
+        socialResponsibility: "Responsabilidad social: Consideración de la salud y nutrición de los clientes.",
+
+        // BeetChips
+        missionTextBeetChips: "Llevar el sabor único de la remolacha a todo el mundo, ofreciendo snacks nutritivos y deliciosos.",
+        visionTextBeetChips: "Ser reconocidos como la marca líder en snacks de remolacha, destacando por nuestra innovación y calidad.",
+        productsTitle: "Nuestros Productos",
+        product1NameBeetChips: "BeetChips",
+        product1DescriptionBeetChips: "Beetchips: Adobadas con amor, disfrutadas por todos.",
+        product2NameBeetChips: "BeetChips",
+        product2DescriptionBeetChips: "Beetchips: Papas seleccionadas, chips perfectos, sabor inigualable.",
+        cartTitle: "Carrito de Compras",
+        cartTotal: "Total",
+        checkoutButton: "Finalizar Compra",
+        passion: "Pasión: Poner dedicación y amor en cada snack para brindar una experiencia única.",
+        teamwork: "Trabajo en equipo: Colaborar para crear un producto de alta calidad.",
+        responsibility: "Responsabilidad: Garantizar procesos de producción seguros y sostenibles.",
+        trust: "Confianza: Para construir una relación sólida con los clientes.",
+
+        // CornRose
+        missionTextCornRose: "Llevar delicia mexicana al paladar extranjero.",
+        visionTextCornRose: "Ser reconocida por su sabor en toda América del norte, ofreciendo galletas con un auténtico toque mexicano.",
+        product1NameCornRose: "CornRose",
+        product1DescriptionCornRose: "CornRose: Un toque emotivo que transmite la pasión y el cuidado que se pone en cada galleta.",
+        product2NameCornRose: "CornRose",
+        product2DescriptionCornRose: "CornRose: el sabor del maíz en cada bocado",
+
+        // Contacto
+        contactTitle: "Contacto",
+        contactDescription: "¿Tienes alguna pregunta, comentario o sugerencia?",
+        contactFormTitle: "Formulario de Contacto",
+        nameLabel: "Nombre:",
+        emailLabel: "Correo Electrónico:",
+        subjectLabel: "Asunto:",
+        messageLabel: "Mensaje:",
+        submitButton: "Enviar Mensaje",
+        contactInfoTitle: "Información de Contacto",
+        addressLabel: "Dirección:",
+        address: "Av. Peñuelas 3, San Pedrito Peñuelas I, 76148 Santiago de Querétaro, Qro.",
+        phoneLabel: "Teléfono:",
+        phone: "+52 442 878 3165",
+        emailContactLabel: "Correo Electrónico:",
+        emailContact: "contacto@nutrisnack.com",
+        locationTitle: "Ubicación"
+    },
+    en: {
+        // ... (otros textos)
+        facebookText: "Follow us on Facebook",
+        // Textos comunes
+        home: "Home",
+        products: "Products",
+        contact: "Contact",
+        beetchips: "BeetChips",
+        cornrose: "CornRose",
+        terms: "Terms and Conditions",
+        copyright: "© NutriSnack. All rights reserved.",
+
+        // Index (NutriSnack)
+        missionTitle: "Mission",
+        missionText: "Share the true flavor of Mexico through delicious snacks, supporting local producers and celebrating our culture.",
+        visionTitle: "Vision",
+        visionText: "To be an internationally recognized exporting company, known for its variety of healthy snacks and commitment to sustainability.",
+        valuesTitle: "Values",
+        authenticity: "Authenticity: We highlight the essence of Mexico in every product.",
+        sustainability: "Sustainability: Through the use of eco-friendly packaging.",
+        innovation: "Innovation: We evolve to meet the needs of the global market.",
+        honesty: "Honesty: Being transparent about the ingredients we use.",
+        socialResponsibility: "Social Responsibility: Considering the health and nutrition of our customers.",
+
+        // BeetChips
+        missionTextBeetChips: "Bring the unique flavor of beets to the world, offering nutritious and delicious snacks.",
+        visionTextBeetChips: "To be recognized as the leading brand in beet snacks, known for our innovation and quality.",
+        productsTitle: "Our Products",
+        product1NameBeetChips: "BeetChips",
+        product1DescriptionBeetChips: "Beetchips: Marinated with love, enjoyed by all.",
+        product2NameBeetChips: "BeetChips",
+        product2DescriptionBeetChips: "Beetchips: Selected potatoes, perfect chips, unmatched flavor.",
+        cartTitle: "Shopping Cart",
+        cartTotal: "Total",
+        checkoutButton: "Checkout",
+        passion: "Passion: Putting dedication and love into every snack to provide a unique experience.",
+        teamwork: "Teamwork: Collaborating to create a high-quality product.",
+        responsibility: "Responsibility: Ensuring safe and sustainable production processes.",
+        trust: "Trust: Building a strong relationship with customers.",
+
+        // CornRose
+        missionTextCornRose: "Bring Mexican delight to foreign palates.",
+        visionTextCornRose: "To be recognized for its flavor throughout North America, offering cookies with an authentic Mexican touch.",
+        product1NameCornRose: "CornRose",
+        product1DescriptionCornRose: "CornRose: An emotional touch that conveys the passion and care put into each cookie.",
+        product2NameCornRose: "CornRose",
+        product2DescriptionCornRose: "CornRose: the flavor of corn in every bite",
+
+        // Contacto
+        contactTitle: "Contact",
+        contactDescription: "Do you have any questions, comments or suggestions?",
+        contactFormTitle: "Contact Form",
+        nameLabel: "Name:",
+        emailLabel: "Email:",
+        subjectLabel: "Subject:",
+        messageLabel: "Message:",
+        submitButton: "Send Message",
+        contactInfoTitle: "Contact Information",
+        addressLabel: "Address:",
+        address: "Av. Peñuelas 3, San Pedrito Peñuelas I, 76148 Santiago de Querétaro, Qro.",
+        phoneLabel: "Phone:",
+        phone: "+52 442 878 3165",
+        emailContactLabel: "Email:",
+        emailContact: "contact@nutrisnack.com",
+        locationTitle: "Location"
+    },
+    fr: {
+        // ... (otros textos)
+        facebookText: "Suivez-nous sur Facebook",
+        // Textos comunes
+        home: "Accueil",
+        products: "Produits",
+        contact: "Contact",
+        beetchips: "BeetChips",
+        cornrose: "CornRose",
+        terms: "Termes et Conditions",
+        copyright: "© NutriSnack. Tous droits réservés.",
+
+        // Index (NutriSnack)
+        missionTitle: "Mission",
+        missionText: "Partager la véritable saveur du Mexique à travers des snacks délicieux, en soutenant les producteurs locaux et en célébrant notre culture.",
+        visionTitle: "Vision",
+        visionText: "Devenir une entreprise exportatrice reconnue internationalement, connue pour sa variété de snacks sains et son engagement envers la durabilité.",
+        valuesTitle: "Valeurs",
+        authenticity: "Authenticité: Nous mettons en valeur l'essence du Mexique dans chaque produit.",
+        sustainability: "Durabilité: Grâce à l'utilisation d'emballages écologiques.",
+        innovation: "Innovation: Nous évoluons pour répondre aux besoins du marché mondial.",
+        honesty: "Honnêteté: Être transparent sur les ingrédients que nous utilisons.",
+        socialResponsibility: "Responsabilité sociale: Prendre en compte la santé et la nutrition de nos clients.",
+
+        // BeetChips
+        missionTextBeetChips: "Apporter la saveur unique des betteraves au monde entier, en offrant des snacks nutritifs et délicieux.",
+        visionTextBeetChips: "Être reconnue comme la marque leader en snacks de betteraves, connue pour notre innovation et notre qualité.",
+        productsTitle: "Nos Produits",
+        product1NameBeetChips: "BeetChips",
+        product1DescriptionBeetChips: "Beetchips: Marinées avec amour, appréciées par tous.",
+        product2NameBeetChips: "BeetChips",
+        product2DescriptionBeetChips: "Beetchips: Pommes de terre sélectionnées, chips parfaites, saveur inégalée.",
+        cartTitle: "Panier d'Achats",
+        cartTotal: "Total",
+        checkoutButton: "Finaliser l'Achat",
+        passion: "Passion: Mettre du dévouement et de l'amour dans chaque snack pour offrir une expérience unique.",
+        teamwork: "Travail d'équipe: Collaborer pour créer un produit de haute qualité.",
+        responsibility: "Responsabilité: Garantir des processus de production sûrs et durables.",
+        trust: "Confiance: Bâtir une relation solide avec les clients.",
+
+        // CornRose
+        missionTextCornRose: "Apporter la délicieuse saveur mexicaine aux palais étrangers.",
+        visionTextCornRose: "Être reconnue pour son goût dans toute l'Amérique du Nord, en offrant des biscuits avec une touche mexicaine authentique.",
+        product1NameCornRose: "CornRose",
+        product1DescriptionCornRose: "CornRose: Une touche émotionnelle qui transmet la passion et le soin apporté à chaque biscuit.",
+        product2NameCornRose: "CornRose",
+        product2DescriptionCornRose: "CornRose: la saveur du maïs à chaque bouchée",
+
+        // Contacto
+        contactTitle: "Contact",
+        contactDescription: "Avez-vous des questions, des commentaires ou des suggestions?",
+        contactFormTitle: "Formulaire de Contact",
+        nameLabel: "Nom:",
+        emailLabel: "Email:",
+        subjectLabel: "Sujet:",
+        messageLabel: "Message:",
+        submitButton: "Envoyer le Message",
+        contactInfoTitle: "Informations de Contact",
+        addressLabel: "Adresse:",
+        address: "Av. Peñuelas 3, San Pedrito Peñuelas I, 76148 Santiago de Querétaro, Qro.",
+        phoneLabel: "Téléphone:",
+        phone: "+52 442 878 3165",
+        emailContactLabel: "Email:",
+        emailContact: "contact@nutrisnack.com",
+        locationTitle: "Localisation"
+    }
+};
+
+function changeLanguage(lang) {
+    // Traducir textos comunes
+    document.getElementById('home').textContent = translations[lang].home;
+    document.getElementById('products').textContent = translations[lang].products;
+    document.getElementById('contact').textContent = translations[lang].contact;
+    document.getElementById('terms').textContent = translations[lang].terms;
+    document.querySelector('footer p').innerHTML = `&copy; ${new Date().getFullYear()} NutriSnack. ${translations[lang].copyright.split('©')[1]}`;
+
+    // Función auxiliar para traducir elementos si existen
+    const translateElement = (id, text) => {
+        const element = document.getElementById(id);
+        if (element) element.textContent = text;
+    };
+
+    // Traducir elementos específicos de cada página
+    translateElement('facebook-link', translations[lang].facebookText);
+    translateElement('mission-title', translations[lang].missionTitle);
+    translateElement('mission-text', translations[lang].missionText);
+    translateElement('vision-title', translations[lang].visionTitle);
+    translateElement('vision-text', translations[lang].visionText);
+    translateElement('values-title', translations[lang].valuesTitle);
+    translateElement('authenticity', translations[lang].authenticity);
+    translateElement('sustainability', translations[lang].sustainability);
+    translateElement('innovation', translations[lang].innovation);
+    translateElement('honesty', translations[lang].honesty);
+    translateElement('social-responsibility', translations[lang].socialResponsibility);
+    
+    // BeetChips
+    translateElement('mission-text-beetchips', translations[lang].missionTextBeetChips);
+    translateElement('vision-text-beetchips', translations[lang].visionTextBeetChips);
+    translateElement('products-title', translations[lang].productsTitle);
+    translateElement('product1-name', translations[lang].product1NameBeetChips);
+    translateElement('product1-description', translations[lang].product1DescriptionBeetChips);
+    translateElement('product2-name', translations[lang].product2NameBeetChips);
+    translateElement('product2-description', translations[lang].product2DescriptionBeetChips);
+    translateElement('cart-title', translations[lang].cartTitle);
+    translateElement('cart-total-label', translations[lang].cartTotal);
+    translateElement('checkout-button', translations[lang].checkoutButton);
+    translateElement('passion', translations[lang].passion);
+    translateElement('teamwork', translations[lang].teamwork);
+    translateElement('responsibility', translations[lang].responsibility);
+    translateElement('trust', translations[lang].trust);
+
+    // CornRose
+    translateElement('mission-text-cornrose', translations[lang].missionTextCornRose);
+    translateElement('vision-text-cornrose', translations[lang].visionTextCornRose);
+    translateElement('product1-name', translations[lang].product1NameCornRose);
+    translateElement('product1-description', translations[lang].product1DescriptionCornRose);
+    translateElement('product2-name', translations[lang].product2NameCornRose);
+    translateElement('product2-description', translations[lang].product2DescriptionCornRose);
+
+    // Contacto
+    translateElement('contact-title', translations[lang].contactTitle);
+    translateElement('contact-description', translations[lang].contactDescription);
+    translateElement('contact-form-title', translations[lang].contactFormTitle);
+    translateElement('name-label', translations[lang].nameLabel);
+    translateElement('email-label', translations[lang].emailLabel);
+    translateElement('subject-label', translations[lang].subjectLabel);
+    translateElement('message-label', translations[lang].messageLabel);
+    translateElement('submit-button', translations[lang].submitButton);
+    translateElement('contact-info-title', translations[lang].contactInfoTitle);
+    translateElement('address-label', translations[lang].addressLabel);
+    translateElement('address', translations[lang].address);
+    translateElement('phone-label', translations[lang].phoneLabel);
+    translateElement('phone', translations[lang].phone);
+    translateElement('email-contact-label', translations[lang].emailContactLabel);
+    translateElement('email-contact', translations[lang].emailContact);
+    translateElement('location-title', translations[lang].locationTitle);
+}
+
+// Resto del código (carrito, carrusel, etc.) permanece igual
+document.getElementById('year').textContent = new Date().getFullYear();
+
+let cart = [];
+let total = 0;
+
+function addToCart(productName, price) {
+    cart.push({ name: productName, price: price });
+    total += price;
+    updateCart();
+}
+
+function updateCart() {
+    const cartItems = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
+
+    cartItems.innerHTML = '';
+
+    cart.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.name} - $${item.price.toFixed(2)}`;
+        cartItems.appendChild(li);
+    });
+
+    cartTotal.textContent = total.toFixed(2);
+}
+
+function checkout() {
+    if (cart.length === 0) {
+        alert('Tu carrito está vacío. Agrega productos antes de finalizar la compra.');
+    } else {
+        alert(`Compra finalizada. Total: $${total.toFixed(2)}`);
+        cart = [];
+        total = 0;
+        updateCart();
+    }
+}
+
+document.getElementById('year').textContent = new Date().getFullYear();
+
+const carouselSlide = document.querySelector('.carousel-slide');
+const leftImage = document.getElementById('left-image');
+const rightImage = document.getElementById('right-image');
+
+const images = [
+    { left: 'imagenes/imagen007.jpg', center: 'imagenes/imagen001.jpg', right: 'imagenes/quesoBeetChips.jpg' },
+    { left: 'imagenes/imagen005.jpg', center: 'imagenes/imagen002.jpg', right: 'imagenes/imagen009.jpg' },
+    { left: 'imagenes/imagen009.jpg', center: 'imagenes/imagen003.jpg', right: 'imagenes/imagen010.jpg' },
+    { left: 'imagenes/imagen010.jpg', center: 'imagenes/imagen004.jpg', right: 'imagenes/imagen005.jpg' },
+    { left: 'imagenes/quesoBeetChips.jpg', center: 'imagenes/imagen005.jpg', right: 'imagenes/imagen004.jpg' }
+];
+
+let currentIndex = 0;
+
+function moveCarousel() {
+    const offset = -currentIndex * 100;
+    carouselSlide.style.transform = `translateX(${offset}%)`;
+
+    // Actualizar las imágenes laterales
+    leftImage.src = images[currentIndex].left;
+    rightImage.src = images[currentIndex].right;
+
+    currentIndex = (currentIndex + 1) % images.length;
+
+    setTimeout(moveCarousel, 3000);
+}
+
+moveCarousel();
